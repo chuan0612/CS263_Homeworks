3e2f2084e8a68f8e0a11e95eab6513ea7eca8933
chuan0612 <chuan.b0612@msa.hinet.net>
2016-03-18 11:39:12 +0800

    homework

---
+++ Homework1/example01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example01
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Student someone =
+                new Student
+                {
+                    name = "老張",
+                    info = new Contact
+                    {
+                         phone = "0938123456",
+                         email = "test@gmail.com"
+                    },               
+                };
+            Console.WriteLine(someone.name);
+            Console.WriteLine(someone.info.email);
+            Console.WriteLine(someone.info.phone);
+        }
+    }
+}
