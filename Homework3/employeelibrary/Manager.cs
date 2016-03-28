2bb6300b2763a007312511ced92740d4a515b672
chuan0612 <chuan.b0612@msa.hinet.net>
2016-03-28 21:22:32 +0800

    第一個視窗作業

---
+++ Homework3/employeelibrary/Manager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeLibrary
+{
+    public class Manager:Employee
+    {
+        private int performanceCount;
+        private int mangerBouns;
+
+        public int MangerBouns
+        {
+            get { return mangerBouns; }
+            set { mangerBouns = value; }
+        }
+
+        public Manager(string id, string name, int baseSalary, int bouns)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.BaseSalary = baseSalary;
+            this.mangerBouns = bouns;
+        }
+       
+        public Manager(string id, string name, string baseSalary, string bouns)
+            : this(id, name, int.Parse(baseSalary), int.Parse(bouns))
+        {
+        }
+
+        virtual public double BaseSalary
+        {
+            get { return this.baseSalary; }
+            set
+            {
+                this.baseSalary = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + string.Format("\n總薪:{0}"
+                , this.mangerBouns + this.baseSalary);
+        }
+    }
+}
