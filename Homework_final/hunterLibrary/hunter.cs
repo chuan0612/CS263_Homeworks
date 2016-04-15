8c68a6ed01a6a3e7bf040ccd447d858b84e5a04c
chuan0612 <chuan.b0612@msa.hinet.net>
2016-04-15 16:58:45 +0800

    作業

---
+++ Homework_final/hunterLibrary/hunter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MonsterLibrary;
+
+namespace hunterLibrary
+{
+    public class hunter : public  monster
+    {
+        public hunter(string name, int blood)
+        {
+            this.name = "Joe";
+            this.blood = 100;
+        }
+    }
+}
