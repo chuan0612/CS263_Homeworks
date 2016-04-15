afac7f8f5103dba4a41cba5b2fd511dcff77c279
chuan0612 <chuan.b0612@msa.hinet.net>
2016-04-15 17:04:51 +0800

    作業

---
+++ Homework_final/myForm/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace myForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.mouseX = new System.Windows.Forms.TextBox();
+            this.mouseY = new System.Windows.Forms.TextBox();
+            this.monsterPositionY = new System.Windows.Forms.TextBox();
+            this.monsterPositionX = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.startButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mouseX
+            // 
+            this.mouseX.Location = new System.Drawing.Point(122, 525);
+            this.mouseX.Name = "mouseX";
+            this.mouseX.Size = new System.Drawing.Size(118, 22);
+            this.mouseX.TabIndex = 0;
+            this.mouseX.Visible = false;
+            // 
+            // mouseY
+            // 
+            this.mouseY.Location = new System.Drawing.Point(-1, 525);
+            this.mouseY.Name = "mouseY";
+            this.mouseY.Size = new System.Drawing.Size(117, 22);
+            this.mouseY.TabIndex = 1;
+            this.mouseY.Visible = false;
+            // 
+            // monsterPositionY
+            // 
+            this.monsterPositionY.Location = new System.Drawing.Point(-1, 497);
+            this.monsterPositionY.Name = "monsterPositionY";
+            this.monsterPositionY.Size = new System.Drawing.Size(117, 22);
+            this.monsterPositionY.TabIndex = 4;
+            this.monsterPositionY.Visible = false;
+            // 
+            // monsterPositionX
+            // 
+            this.monsterPositionX.Location = new System.Drawing.Point(122, 497);
+            this.monsterPositionX.Name = "monsterPositionX";
+            this.monsterPositionX.Size = new System.Drawing.Size(118, 22);
+            this.monsterPositionX.TabIndex = 3;
+            this.monsterPositionX.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(508, 518);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(64, 31);
+            this.startButton.TabIndex = 5;
+            this.startButton.Text = "開始";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(532, 444);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(344, 524);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(161, 22);
+            this.textBox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.monsterPositionY);
+            this.Controls.Add(this.monsterPositionX);
+            this.Controls.Add(this.mouseY);
+            this.Controls.Add(this.mouseX);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox mouseX;
+        private System.Windows.Forms.TextBox mouseY;
+        private System.Windows.Forms.TextBox monsterPositionY;
+        private System.Windows.Forms.TextBox monsterPositionX;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Homework_final/myForm/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MonsterLibrary;
+
+namespace myForm
+{
+    public partial class Form1 : Form
+    {
+        monster bigMonster;
+       
+        public Random random = new Random();
+        public int myblood;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            bigMonster = new monster("大怪物",50);
+            myblood = 25;
+         }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+                
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            mouseX.Text = "X : " + e.X;
+            mouseY.Text = "Y : " + e.Y;
+            textBox1.Text = "沒打中,怪獸反擊!";
+            myblood -= 5;
+            if(myblood == 0)
+                textBox1.Text = "我死了";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int resultx = random.Next(1, 499);
+            int resulty = random.Next(1, 499);
+            monsterPositionX.Text = "X : " + resultx;
+            monsterPositionY.Text = "Y : " + resulty;
+
+            if (checkAlive())
+            {
+                pictureBox1.Left = resultx;
+                pictureBox1.Top = resulty;
+            }
+            textBox1.Text = " ";
+            
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+            startButton.Text = timer1.Enabled ? "結束" : "開始";
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = "打中";
+            bigMonster.blood -= 5;
+            if (!checkAlive())
+            {
+                textBox1.Text = bigMonster.name + "死了!";
+                //Close();
+            }
+       }
+
+        public  bool checkAlive()
+        {
+            if (bigMonster.blood == 0)
+                return false;
+            else
+                return true;
+        }
+    }
+}
